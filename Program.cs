0d2cf52bdb792795039fdcbc663d89093d12f2cf
p22105710 <p22105710@IUTA-WS-TP07-09>
2022-05-11 16:40:54 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TD_9_Qualité_de_dev
+{
+    class Program
+    {
+        public static int AdditionDeuxEntiers (int nb1, int nb2)
+        {
+            int res;
+            res = nb1 + nb2;
+            return res;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(AdditionDeuxEntiers(2, 3));
+        }
+    }
+}
